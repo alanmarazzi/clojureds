f372988403c5b6a431c16049a4c89d0d69effdaf
Alan Marazzi <alan.marazzi@gmail.com>
2017-11-03 17:41:31 +0100

    Update chapter 2 up to 2 tailed t-test

--- ch2/src/cljds/ch2/examples.clj
+++ ch2/src/cljds/ch2/examples.clj
@@ -82,3 +82,87 @@
          (with-parsed-date)
          (filtered-times {:date {:$eq may-1}})
          (standard-error))))
+
+; Confidence intervals
+(defn ex-2-9
+  []
+  (let [may-1 (f/parse-local-date "2015-05-01")]
+    (->> (load-data "dwell-times.tsv")
+         (with-parsed-date)
+         (filtered-times {:date {:$eq may-1}})
+         (confidence-interval 0.95))))
+
+; Sample comparison
+(defn ex-2-10
+  []
+  (let [times (->> (load-data "campaign-sample.tsv")
+                   (i/$ :dwell-time))]
+    (println "n:      " (count times))
+    (println "Mean:   " (s/mean times))
+    (println "Median: " (s/median times))
+    (println "SD:     " (s/sd times))
+    (println "SE:     " (standard-error times))))
+
+(defn ex-2-11
+  []
+  (->> (load-data "campaign-sample.tsv")
+       (i/$ :dwell-time)
+       (confidence-interval 0.95)))
+
+(defn ex-2-12
+  []
+  (let [means (->> (load-data "dwell-times.tsv")
+                   (with-parsed-date)
+                   (mean-dwell-times-by-date)
+                   (i/$ :dwell-time))]
+    (-> (c/histogram means
+                     :x-label "Daily mean"
+                     :nbins 20)
+        (i/view))))
+
+(defn ex-2-13
+  []
+  (let [weekend-times (->> (load-data "dwell-times.tsv")
+                           (with-parsed-date)
+                           (i/$where {:date {:$fn p/weekend?}})
+                           (i/$ :dwell-time))]
+    (println "n:      " (count weekend-times))
+    (println "Mean:   " (s/mean weekend-times))
+    (println "Median: " (s/median weekend-times))
+    (println "SD:     " (s/sd weekend-times))
+    (println "SE:     " (standard-error weekend-times))))
+
+(defn ex-2-14
+  []
+  (let [data (->> (load-data "new-site.tsv")
+                  (:rows)
+                  (group-by :site)
+                  (map-vals (partial map :dwell-time)))
+        a (get data 0)
+        b (get data 1)]
+    (println "a n: " (count a))
+    (println "b n: " (count b))
+    (println "z-stat: " (z-stat a b))
+    (println "p-value: " (z-test a b))))
+
+(defn ex-2-15
+  []
+  (let [data (->> (load-data "new-site.tsv")
+                  (:rows)
+                  (group-by :site)
+                  (map-vals (partial map :dwell-time)))
+        a (get data 0)
+        b (get data 1)]
+    (t-stat a b)))
+
+(defn ex-2-16
+  "One tail t-test"
+  []
+  (let [data (->> (load-data "new-site.tsv")
+                  (:rows)
+                  (group-by :site)
+                  (map-vals (partial map :dwell-time)))
+        a (get data 0)
+        b (get data 1)]
+    (t-test a b)))
+
--- ch2/src/cljds/ch2/stats.clj
+++ ch2/src/cljds/ch2/stats.clj
@@ -23,3 +23,38 @@
   [xs]
   (/ (standard-deviation xs)
      (Math/sqrt (count xs))))
+
+(defn confidence-interval
+  [p xs]
+  (let [x-bar (s/mean xs)
+        se (standard-error xs)
+        z-crit (s/quantile-normal (- 1 (/ (- 1 p) 2)))]
+    [(- x-bar (* se z-crit))
+     (+ x-bar (* se z-crit))]))
+
+(defn pooled-standard-error
+  [a b]
+  (i/sqrt (+ (/ (i/sq (standard-deviation a)) (count a))
+             (/ (i/sq (standard-deviation b)) (count b)))))
+
+(defn pooled-standard-error
+  [a b]
+  (i/sqrt (+ (i/sq (standard-error a))
+             (i/sq (standard-error b)))))
+
+(defn z-stat
+  [a b]
+  (-> (- (mean a)
+         (mean b))
+      (/ (pooled-standard-error a b))))
+
+(defn z-test
+  [a b]
+  (s/cdf-normal (z-stat a b)))
+
+(def t-stat z-stat)
+
+(defn t-test
+  [a b]
+  (let [df (+ (count a) (count b) -2)]
+    (- 1 (s/cdf-t (i/abs (t-stat a b)) :df df))))
